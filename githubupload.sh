df9a2c5738dbef81d5c5bad99a1b3095df080ca5
Jelle Victoor <jelle@victoor.io>
2021-10-27 19:53:25 +0100

    config

---
+++ githubupload.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+uploadbool=false
+today=`date +%Y-%m-%d.%H:%M:%S`
+backupdir="backup/"
+
+cd ~/klipper_config
+
+git pull origin Voron_V2.1442
+
+for file in *[0-9].cfg; do
+        if [ -f "$file" ] # does file exist?
+        then
+		   mv $file $backupdir$file
+		   uploadbool=true
+        fi
+done
+
+git add .
+git commit -m "backup."$today
+git push -u origin Voron_V2.1442 --force
